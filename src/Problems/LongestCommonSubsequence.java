579b50ba26d9cd92fe2776c17ef9ecb743f297e4
Salaikumar <saravanan1115@gmail.com>
2014-06-01 19:42:36 +0530

    Proper Dynamic Programming approach is implemented. dynamicLCS is the method.

--- src/Problems/LongestCommonSubsequence.java
+++ src/Problems/LongestCommonSubsequence.java
@@ -1,11 +1,10 @@
 /*
  * Implementation of Longest common subsequence using Dynamic Programming
+ *  
+ *  Longest Subsequence length is working properly. But the subsequence is not working properly
  */
 
 package Problems;
-
-import java.util.Queue;
-import java.util.Stack;
 
 
 public class LongestCommonSubsequence {
@@ -16,7 +15,7 @@ public class LongestCommonSubsequence {
 	private StringBuilder output;
 
 	// Stack to hold the Longest common Subsequence
-//	private Stack<Character> lcs;
+	// private Stack<Character> lcs;
 
 	// LCS length
 	private int length[][];
@@ -26,9 +25,9 @@ public class LongestCommonSubsequence {
 		this.a = a;
 		this.b = b;
 		length = new int[a.length()][b.length()];
-//		lcs = new Stack<Character>();
-		//  Create it with shorter length
-		output = new StringBuilder(a.length() < b.length()? a.length():b.length());
+		// lcs = new Stack<Character>();
+		// Create it with shorter length
+		output = new StringBuilder();
 		/*
 		 * Making the i,0 and 0,j pair value as Zero
 		 */
@@ -42,11 +41,13 @@ public class LongestCommonSubsequence {
 		 * for (int i = 0; i < b.length(); i++) length[0][i] = 0;
 		 */
 
-		calculateLCS(a.length() - 1, b.length() - 1);
+		// calculateLCS(a.length() - 1, b.length() - 1);
+		dynamicLCS();
 	}
 
 	/*
-	 * Calculate the LCS
+	 * Calculate the LCS Made it recursive, Haven't used any dynamic programming
+	 * technique
 	 */
 	private int calculateLCS(int l1, int l2) {
 
@@ -56,7 +57,7 @@ public class LongestCommonSubsequence {
 
 		if (a.charAt(l1) == b.charAt(l2)) {
 			length[l1][l2] = 1 + calculateLCS(l1 - 1, l2 - 1);
-//			output.append(a.charAt(l1));
+			// output.append(a.charAt(l1));
 		}
 
 		else {
@@ -67,6 +68,52 @@ public class LongestCommonSubsequence {
 		return length[l1][l2];
 
 	}
+
+	/*
+	 * Implementation using Dynamic Programming
+	 */
+	private void dynamicLCS() {
+
+		// run a seperate Loop for 0 pairs
+		// n,0
+		char c = a.charAt(0);
+		for (int j = 0; j < b.length(); j++) {
+			if (c == b.charAt(j)) {
+				length[0][j] = 1;
+				output.append(c);
+			} else {
+				length[0][j] = 0;
+			}
+		}
+
+		// m,0
+		c = b.charAt(0);
+		for (int j = 1; j < a.length(); j++) {
+			if (c == a.charAt(j)) {
+				length[j][0] = 1;
+				output.append(c);
+			} else {
+				length[j][0] = 0;
+			}
+		}
+
+		for (int i = 1; i < a.length(); i++) {
+			for (int j = 1; j < b.length(); j++) {
+				if (a.charAt(i) == b.charAt(j)) {
+					length[i][j] = 1 + length[i - 1][j - 1];
+					output.append(a.charAt(i));
+				} else {
+					length[i][j] = length[i][j - 1] > length[i - 1][j] ? length[i][j - 1]
+							: length[i - 1][j];
+				}
+			}
+		}
+		
+	}
+
+	/*
+	 * 
+	 */
 
 	/*
 	 * Return the LCS Length
@@ -101,20 +148,22 @@ public class LongestCommonSubsequence {
 	 */
 	public static void main(String[] args) {
 
-		String a = "ACDEFGMNSUXDHERERJVF";
-		String b = "DEFGXF";
+		 String a = "ACDEFGMNSUXDHERERJVF";
+		 String b = "MNSDEFGXF";
 
+//		String a = "ABCDEF";
+//		String b = "ABAX";
 		System.out.println("A.length = " + a.length());
 		System.out.println("b.length = " + b.length());
 
 		LongestCommonSubsequence lcs = new LongestCommonSubsequence(a, b);
+		
+		System.out.println("LCS Length ="+lcs.getLCSLength(5, 5));
 
-		// System.out.println("LCS Length ="+lcs.getLCSLength(a.length(),
-		// b.length()));
+		System.out.println("LCS Length ="
+				+ lcs.getLCSLength(a.length(), b.length()));
 
-		System.out.println("LCS Length =" + lcs.getLCSLength(a.length(), b.length()));
-
-	//	System.out.println("String = " + lcs.getLCS());
+		System.out.println("String = " + lcs.getLCS());
 
 		// Print the complete Lengths
 
